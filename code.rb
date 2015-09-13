f4e12a49378ab68c88adcab2cda9b1b7c3d45a01
ipatriots <nicolin16@gmail.com>
2015-09-13 12:49:57 -0400

    initial commit

---
+++ code.rb
@@ -0,0 +1,39 @@
+# <<-DOC
+#
+# HTTP Challenge
+# --------------
+#
+# * Define the common HTTP verbs
+#   - GET:
+#   - POST:
+#   - PUT/PATCH:
+#   - DELETE:
+#
+# * Use the tool of your choice to draw the HTTP request/response cycle.
+#   - include a link to your diagram, here
+#
+# * Use telnet, curl, or ruby code to retrieve the messages at
+#   launch-academy-chat.herokuapp.com/messages
+#
+# * Use telnet, curl, or ruby code to create a new message at
+#   launch-academy-chat.herokuapp.com/messages
+#
+#   - Kernel::system method in ruby will allow you to execute command-line utilities such as curl.
+#   - other useful ruby libraries:
+#     * Net::HTTP
+#     * Net::Telnet
+#
+# DOC
+
+# code goes here
+system ("curl http://launch-academy-chat.herokuapp.com/messages")
+system ("curl --verbose -d content='woo woo it worked!' http://launch-academy-chat.herokuapp.com/messages")
+
+puts "
+------------                    respond        ------------
+|           |  <+++++++++++++++++++++++++++++ |    the    |
+|           |                                 | server    |
+|the        |  +++++++++++++++++++++++++++++> |           |
+|computer   |           request               |           |
+------------                                  ------------
+"
